b352a53c2d0477a82ed212d735a8742ce9cb0ade
Berry Phillips <berryphillips@gmail.com>
2022-06-20 23:17:23 +0900

    Updates

--- home/config-files.nix
+++ home/config-files.nix
@@ -1,4 +1,8 @@
-{ config, pkgs, lib, ... }:
-
+{ config, lib, ... }:
+let
+  inherit (config.lib.file) mkOutOfStoreSymlink;
+  inherit (config.home.user-info) nixConfigDirectory;
+in
 {
+  xdg.configFile."direnv/config.toml".source = mkOutOfStoreSymlink "${nixConfigDirectory}/configs/direnv/config.toml";
 }
