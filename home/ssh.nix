4044c7f803671b735940b6e853bd5f9df30fb123
Berry Phillips <berryphillips@gmail.com>
2022-08-03 17:04:16 +0900

    Updates

--- home/ssh.nix
+++ home/ssh.nix
@@ -5,7 +5,7 @@
   programs.ssh.matchBlocks = {
     "*" = {
       extraOptions = {
-        IdentityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+        IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
       };
 
     };
