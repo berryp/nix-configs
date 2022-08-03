4f1f2df61c163750323b83807822b2d7f0ffd99e
Berry Phillips <berryphillips@gmail.com>
2022-08-03 17:01:24 +0900

    Updates

--- home/ssh.nix
+++ home/ssh.nix
@@ -3,6 +3,12 @@
 {
   programs.ssh.enable = true;
   programs.ssh.matchBlocks = {
+    "*" = {
+      extraOptions = {
+        IdentityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+      };
+
+    };
     "github.com" = {
       hostname = "ssh.github.com";
       port = 443;
