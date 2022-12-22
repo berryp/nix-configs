3e06fad88006abbcc284f4c4426e2dd9e147a5bc
Berry Phillips <berry@berryp.xyz>
2022-12-22 10:02:48 +0900

    Use 1Password for signing

--- home/git.nix
+++ home/git.nix
@@ -1,26 +1,41 @@
 { config, pkgs, ... }:
 
+let
+  signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC2o35XUfVCZPxvsxowdfoY5+g4/P8Kz/ufkb81wMmuT";
+in
 {
   # Git
   # https://rycee.gitlab.io/home-manager/options.html#opt-programs.git.enable
   # Aliases config in ./configs/git-aliases.nix
-  programs.git.enable = true;
 
-  programs.git.extraConfig = {
-    diff.colorMoved = "default";
-    pull.rebase = true;
+  home.file.".ssh/allowed_signers".text =
+    "* ${signingKey}";
+
+  programs.git = {
+    enable = true;
+
+    userEmail = config.home.user-info.email;
+    userName = config.home.user-info.fullName;
+
+    extraConfig = {
+      diff.colorMoved = "default";
+      pull.rebase = true;
+      commit.gpgsign = true;
+      gpg.format = "ssh";
+      gpg.ssh = {
+        allowedSignersFile = "~/.ssh/allowed_signers";
+        program = "${pkgs._1password-gui}/Applications/1Password.app/Contents/MacOS/op-ssh-sign";        
+      };
+      user.signingkey = signingKey;
+    };
+
+    ignores = [
+      ".DS_Store"
+    ];
+
+    # Enhanced diffs
+    delta.enable = true;
   };
-
-  programs.git.ignores = [
-    ".DS_Store"
-  ];
-
-  programs.git.userEmail = config.home.user-info.email;
-  programs.git.userName = config.home.user-info.fullName;
-
-  # Enhanced diffs
-  programs.git.delta.enable = true;
-
 
   # GitHub CLI
   # https://rycee.gitlab.io/home-manager/options.html#opt-programs.gh.enable
